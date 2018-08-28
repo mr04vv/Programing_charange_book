886e4071fa5acf0474567ae11df4c765095b8283
mori takuto <mr04vv@gmail.com>
2018-08-29 00:56:49 +0900

    second commit

---
+++ 1-6_example/1.py
@@ -0,0 +1,17 @@
+bar_num = int(input())
+bar_length_list = [int(input()) for i in range(bar_num)]
+
+
+ans = 0
+
+for i in range(bar_num-2):
+    first = bar_length_list[i]
+    for j in range(i+1,bar_num-1):
+        second = bar_length_list[j]
+        for k in range(j+1,bar_num):
+            third = bar_length_list[k]
+            if (first**2 == second**2 + third**2) or (second**2 == first**2 + third**2) or (third**2 == first**2 + second**2):
+                ans = first+second+third
+            
+
+print(ans)

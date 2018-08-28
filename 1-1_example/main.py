234b81fa047bd64e606fa090ce2c4dfccc6ea09d
mori takuto <mr04vv@gmail.com>
2018-08-29 01:09:53 +0900

    rm no use directory

--- 1-1_example/main.py
+++
@@ -1,9 +0,0 @@
-number_of_times = 4
-number_of_papers = int(input())
-sum = int(input())
-numbers = [input() for i in range(number_of_papers)]
-
-print(number_of_times)
-print(number_of_papers)
-print(sum)
-print(numbers)
